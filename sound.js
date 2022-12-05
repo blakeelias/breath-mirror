e9f14da45aee72b72a5bc3c05292aa7b9dd88575
Blake Elias <blakeelias@gmail.com>
2022-12-04 16:31:25 -0800

    Create basic MIDI playing

---
+++ sound.js
@@ -0,0 +1,33 @@
+var context = null;
+var oscillator = null;
+function getOrCreateContext() {
+  if (!context) {
+    context = new AudioContext();
+    oscillator = context.createOscillator();
+    oscillator.connect(context.destination);
+  }
+  return context;
+  
+}
+
+        var isStarted = false;
+        function playSound(frequency, type) {
+          getOrCreateContext();
+          oscillator.frequency.setTargetAtTime(frequency, context.currentTime, 0);
+          if (!isStarted) {
+            oscillator.start(0);
+            isStarted = true;
+          } else {
+            context.resume();
+          }
+        }
+
+        function stopSound() {
+          context.suspend();
+        }
+
+        document.getElementById('basic').addEventListener('click', playSound.bind(null, 440, 'square'));
+        document.getElementById('basic2').addEventListener('click', playSound.bind(null, 800, 'square'));
+        document.getElementById('basic3').addEventListener('click', playSound.bind(null, 1000, 'square'));
+        document.getElementById('stop').addEventListener('click', stopSound);
+
